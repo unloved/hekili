fd505bc0b508b3ebc3fb889698871c4dd0878148
Alexander Logunov <unlovedru@gmail.com>
2023-06-24 13:56:41 +0300

    Update DeathKnight.lua with unholy_force

--- Wrath/DeathKnight.lua
+++ Wrath/DeathKnight.lua
@@ -833,6 +833,13 @@ spec:RegisterAuras( {
         max_stack = 1,
     },
 
+     -- Increases strength by 200 for 20 sec.
+    unholy_force = {
+      id = 67383,
+      duration = 20,
+      max_stack = 1
+    },
+
     -- Death Runes
     death_rune_1 = {
         duration = 30,
